c3fddbab0058181ca84f1e2d157da98194e031a6
dearpongsakorn <69025502+dearpongsakorn@users.noreply.github.com>
2026-02-19 18:31:55 +0700

    Refactor routes to use StudentController methods

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Models\Student;
+use App\Http\Controllers\StudentController;
 
-Route::get('/', function () {
-    $students = Student::all();
-    return view('home', compact('students'));
-});
+Route::get('/', [StudentController::class, 'index']);
 
-Route::post('/add', function (Request $request) {
-    Student::create($request->all());
-    return redirect('/');
-});
+Route::post('/add', [StudentController::class, 'add']);
 
-Route::post('/update/{id}', function (Request $request, $id) {
-    $student = Student::find($id);
-    $student->update($request->all());
-    return redirect('/');
-});
+Route::post('/update/{id}', [StudentController::class, 'update']);
 
-Route::get('/delete/{id}', function ($id) {
-    Student::destroy($id);
-    return redirect('/');
-});
+Route::get('/delete/{id}', [StudentController::class, 'delete']);
